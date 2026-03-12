79dc174115bc4c5923dfb434b9d1f0ff7bdd6aee
Kevin <krawczynskikewin@gmail.com>
2026-03-12 06:29:02 +0100

    refactor: replace implicit wait with explicit wait for better synchronization

--- src/main/java/com/automation/VegPage.java
+++ src/main/java/com/automation/VegPage.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
 import java.time.Duration;
@@ -17,7 +19,7 @@ public class VegPage {
 
 
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().window().maximize();
 
         String[] itemsNeeded = {"Cucumber", "Brocolli", "Beetroot"};
@@ -27,11 +29,14 @@ public class VegPage {
 
         driver.findElement(By.cssSelector("img[alt='Cart']")).click();
         driver.findElement(By.xpath("//button[normalize-space()='PROCEED TO CHECKOUT']")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("promoCode")));
         driver.findElement(By.className("promoCode")).sendKeys("rahulshettyacademy");
         driver.findElement(By.cssSelector(".promoBtn")).click();
-
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".promoInfo")));
         String actualText = driver.findElement(By.cssSelector(".promoInfo")).getText();
         Assert.assertEquals(actualText, "Code applied ..!");
+        System.out.println(actualText);
 
 
     }
