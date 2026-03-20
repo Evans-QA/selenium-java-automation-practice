e07bea2cfb338a05fe403af395b6e4e42e5e3776
Kevin <krawczynskikewin@gmail.com>
2026-03-20 11:42:03 +0100

    feat: implement dynamic date selection using calendar zoom-out strategy

---
+++ src/main/java/com/automation/CalendarSelectionTest.java
@@ -0,0 +1,38 @@
+package com.automation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class CalendarSelectionTest {
+    public static void main(String[] args) {
+
+        String yearNumber = "2027";
+        String monthNumber = "8";
+        String dayNumber = "19";
+        String[] expectedList = {monthNumber, dayNumber, yearNumber};
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+
+        driver.findElement(By.cssSelector(".react-date-picker__inputGroup")).click();
+
+        driver.findElement(By.cssSelector(".react-calendar__navigation__label")).click();
+
+        driver.findElement(By.cssSelector(".react-calendar__navigation__label")).click();
+
+        driver.findElement(By.xpath("//button[text()='" + yearNumber + "']")).click();
+
+        driver.findElements(By.cssSelector(".react-calendar__year-view__months__month")).get(Integer.parseInt(monthNumber) - 1).click();
+
+        driver.findElement(By.xpath("//abbr[text()='" + dayNumber + "']")).click();
+
+    }
+}
